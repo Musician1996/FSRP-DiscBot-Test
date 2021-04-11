cd13615a2a448e5269aee41b922143938d4a1b4a
Musician1996 <52727307+Musician1996@users.noreply.github.com>
2021-04-11 04:04:59 -0400

    Add files via upload

---
+++ commands/clear.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'clear',
+    description: "Clears Messages!",
+    async execute(message, args) {
+        if(!args[0]) return message.reply("Please Specify The Amount Of Messages To be Deleted!");
+        if(isNaN(args[0])) return message.reply("Please Enter A Number!");
+
+        if(args[0] > 100000) return message.reply("You Cannot Delete More Than 100 Messages!");
+        if(args[0] < 1) return message.reply("You Must be Kidding Me, Delete Atleast One Message For Me To Do Something!");
+
+        await message.channel.messages.fetch({limit: args[0]}).then(messages =>{
+            message.channel.bulkDelete(messages);
+        });
+    }
+}
